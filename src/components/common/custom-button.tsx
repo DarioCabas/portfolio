b60fb122089d911785f78f8cc743ef3fcb3bedf3
Dario <hz-hertzio@hotmail.com>
2022-09-24 10:49:34 -0500

    quite some animation in navbar

--- src/components/common/custom-button.tsx
+++ src/components/common/custom-button.tsx
@@ -18,7 +18,6 @@ const CustomButton: FC<PropsButtonCustom> = (props) => {
     <Box
       component={motion.div}
       whileHover={{ scale: 1.2 }}
-      whileTap={{ scale: 0.9 }}
       transition={{ type: "spring", stiffness: 400, damping: 17 }}
     >
       <Button
